cd785a011cbfdf8ed5cf7d9fce3b7df5b1a03003
Cristian Iturri <iturri.cf@gmail.com>
2018-06-05 02:11:40 -0300

    Refactor importante. Se migró toda la funcionalidad a la nueva UI

--- clusteris/config/view.py
+++ clusteris/config/view.py
@@ -13,12 +13,10 @@ class ConfigView (wx.Dialog):
 
     def __init__(self, parent):
         """Constructor. Initializes the wxPython app and Builds main UI."""
-        self.app = wx.App(0)
-
         self.BuildMainUI(parent)
 
     def BuildMainUI(self, parent):
-        wx.Frame.__init__(self, parent, id = wx.ID_ANY, title = u"ClusteRIS", pos = wx.DefaultPosition, size = wx.Size(-1,-1), style = wx.CAPTION|wx.CLOSE_BOX|wx.MAXIMIZE_BOX|wx.MINIMIZE_BOX|wx.SYSTEM_MENU|wx.TAB_TRAVERSAL)
+        wx.Frame.__init__(self, parent, id = wx.ID_ANY, title = u"Configuración de procesamiento", pos = wx.DefaultPosition, size = wx.Size(-1,-1), style = wx.CAPTION|wx.CLOSE_BOX|wx.MAXIMIZE_BOX|wx.MINIMIZE_BOX|wx.SYSTEM_MENU|wx.TAB_TRAVERSAL)
 
         self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
 
@@ -179,13 +177,13 @@ class ConfigView (wx.Dialog):
         """The action button UI. Start processing the dataset."""
         bSizerAction = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.buttonGraphic = wx.Button(self.panelAction, wx.ID_ANY, u"Graficar", wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizerAction.Add(self.buttonGraphic, 0, wx.ALIGN_CENTER, 5)
+        self.buttonCancel = wx.Button(self.panelAction, wx.ID_ANY, u"Ca&ncelar", wx.DefaultPosition, wx.DefaultSize, 0)
+        bSizerAction.Add(self.buttonCancel, 0, wx.ALIGN_CENTER, 5)
 
         self.buttonProcess = wx.Button(self.panelAction, wx.ID_ANY, u"P&rocesar", wx.DefaultPosition, wx.DefaultSize, 0)
         bSizerAction.Add(self.buttonProcess, 0, wx.ALIGN_CENTER, 5)
 
-        container.Add(bSizerAction, 0, wx.ALIGN_CENTER|wx.ALL, 5)
+        container.Add(bSizerAction, 0, wx.ALIGN_RIGHT|wx.ALL, 5)
 
     def BuildParamsUI(self, container):
         """Processing params."""
@@ -352,9 +350,7 @@ class ConfigView (wx.Dialog):
         """ Initializes the main loop for this UI. Starts listening events."""
         self.sizerMain.Fit(self)
         self.Centre(wx.BOTH)
-        self.Show(True)
-
-        self.app.MainLoop()
+        return self.ShowModal()
 
     def __del__(self):
         pass
